0b7f922e9c00a276d6e3dd04f3004b7a3c6e218d
Iyed-M <iyedmhiri01@gmail.com>
2024-02-04 19:52:57 +0100

    add save and get for json file

--- todo.go
+++ todo.go
@@ -1,7 +1,10 @@
 package todo
 
 import (
+	"encoding/json"
+	"errors"
 	"fmt"
+	"os"
 	"time"
 )
 
@@ -33,4 +36,29 @@ func (l *List) Delete(i int) error {
 	ls := *l
 	*l = append(ls[:index], ls[index+1:]...)
 	return nil
+}
+
+func (l *List) Save(filename string) error {
+	js, err := json.Marshal(l)
+	if err != nil {
+		return err
+	}
+
+	return os.WriteFile(filename, js, 0644)
+}
+
+func (l *List) Get(filename string) error {
+	file, err := os.ReadFile(filename)
+	if err != nil {
+		if errors.Is(err, os.ErrNotExist) {
+			return nil
+		}
+		return err
+	}
+
+	if len(file) == 0 {
+		return nil
+	}
+
+	return json.Unmarshal(file, l)
 }
