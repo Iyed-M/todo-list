0f351efceac8617a3e41116c3c3581874b708dc4
Iyed-M <iyedmhiri01@gmail.com>
2024-02-04 18:52:37 +0100

    init

--- todo.go
+++ todo.go
@@ -1,6 +1,9 @@
 package todo
 
-import "time"
+import (
+	"fmt"
+	"time"
+)
 
 type item struct {
 	Task        string
@@ -10,3 +13,24 @@ type item struct {
 }
 
 type List []item
+
+func (l *List) Add(task string) {
+	t := item{
+		Task:        task,
+		Done:        false,
+		CreatedAt:   time.Now(),
+		CompletedAt: time.Time{},
+	}
+	*l = append(*l, t)
+}
+
+func (l *List) Delete(i int) error {
+	if i < 1 || i > len(*l) {
+		return fmt.Errorf("unvalid item %d", i)
+	}
+
+	index := i - 1
+	ls := *l
+	*l = append(ls[:index], ls[index+1:]...)
+	return nil
+}
